e108502dbfcf8313d107513f6a2a57fecc0bd3f7
vidalpc2010 <vidalpc2010@hotmail.com>
2022-04-03 22:23:03 -0500

    controled component ,  javascript tricks,  hooks

--- src/Components/Expenses/ExpenseItem/ExpenseItem.jsx
+++ src/Components/Expenses/ExpenseItem/ExpenseItem.jsx
@@ -2,33 +2,31 @@ import "./ExpenseItem.css";
 import ExpenseDate from "../ExpenseDate/ExpenseDate";
 import Card from "../../UI/Card/Card";
 
-import React,{ useState } from "react";
+import React, { useState } from "react";
 
 function ExpenseItem(props) {
-  
-  
   //React Hooks , All the hooks stats with the word "use" and all these hooks must only
-  // be called inside of React component functions : 
+  // be called inside of React component functions :
   // 1.  useState
-  const [title,setTitle] = useState(props.title);  
-  
+  const [title, setTitle] = useState(props.title);
 
   // let title = props.title;
 
   const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
+    setTitle("Updated!");
   };
 
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-      <button onClick={clickHandler}>Edit</button>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+        <button onClick={clickHandler}>Edit</button>
+      </Card>
+    </li>
   );
 }
 
