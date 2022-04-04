f5cb4cfecaecca7f69bd85c0adf1e2c77ba30b28
vidalpc2010 <vidalpc2010@hotmail.com>
2022-04-03 22:24:34 -0500

    test commit

--- src/Components/Expenses/ExpensesChart/ExpensesChart.jsx
+++ src/Components/Expenses/ExpensesChart/ExpensesChart.jsx
@@ -21,7 +21,7 @@ const ExpensesChart = (props) => {
   for (const expense of props.expenses) {
     const expenseMonth = expense.date.getMonth();
     chartDataPoints[expenseMonth].value += expense.amount;
-    // console.log(expense.amount);
+    // console.log(expense.amount); 
   }
 
   //   console.log(chartDataPoints);
